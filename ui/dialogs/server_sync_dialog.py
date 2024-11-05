f4ef8cd55ef0ee8aaeaa3bf7d978e7ed169e7943
timmott0 <timothy.mott0@outlookk.com>
2024-11-05 09:23:12 -0500

    bug fix

--- ui/dialogs/server_sync_dialog.py
+++ ui/dialogs/server_sync_dialog.py
@@ -1,4 +1,6 @@
 from PyQt5.QtWidgets import QDialog, QVBoxLayout, QLabel, QPushButton, QProgressBar
+from pathlib import Path
+import os
 
 class ServerSyncDialog(QDialog):
     def __init__(self, config, parent=None):
@@ -15,3 +17,20 @@ class ServerSyncDialog(QDialog):
         self.button = QPushButton("Close")
         self.button.clicked.connect(self.accept)
         layout.addWidget(self.button)
+
+    def update_path_settings(self):
+        """Update path settings to user's home directory after successful connection"""
+        home_dir = str(Path.home())
+        
+        # Update paths in config
+        if 'paths' in self.config:
+            self.config['paths'].update({
+                'config': os.path.join(home_dir, '.config/app'),
+                'keys': os.path.join(home_dir, '.config/app/keys'),
+                'licenses': os.path.join(home_dir, '.config/app/licenses')
+            })
+            
+    def accept(self):
+        # Update paths before closing
+        self.update_path_settings()
+        super().accept()
